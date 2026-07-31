5fa81eaeaac524ce2c1b718321836582d5a0e4cc
akitig <akitig24@gmail.com>
2026-08-01 04:03:31 +0900

    refactor: extract Omikuji repository

--- repositories/__init__.py
+++ repositories/__init__.py
@@ -1,5 +1,6 @@
 """Persistence repositories."""
 
 from repositories.joya_repository import JoyaRepository
+from repositories.omikuji_repository import OmikujiRepository
 
-__all__ = ["JoyaRepository"]
+__all__ = ["JoyaRepository", "OmikujiRepository"]
